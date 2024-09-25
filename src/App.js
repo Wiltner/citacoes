9906811ff794f6da281313749a72cc7c8be8e402
Wiltner <matheuswiltner@gmail.com>
2024-09-24 21:05:22 -0300

    Atividade realizada

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const fetchQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.content);
+        setAuthor(data.author);
+      })
+      .catch((error) => console.error("Erro ao buscar citação:", error));
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Citações Aleatórias</h1>
+        <div id="quote">
+          <p>{quote}</p>
+          <h3>- {author}</h3>
+        </div>
+        <button onClick={fetchQuote}>Nova Citação</button>
       </header>
     </div>
   );
